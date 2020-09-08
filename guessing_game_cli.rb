b97c9310906f22bd0090ec43309a944476697608
Zack Carlson <jzcarlson014@gmail.com>
2020-09-08 01:49:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   num = rand(6) + 1
   guess = gets.chomp 
-  if guess == num
+  if guess.to_i == num
     puts "You guessed the correct number!"
   elsif guess == "exit"
     puts "Goodbye!"
